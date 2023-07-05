44627c1e4a2fe8d5b43531922885b444677d7ca4
G10on <jitenparwani1c17@gmail.com>
2023-07-05 03:03:23 +0200

    Kernel classes reduced with Heritage

--- Processing/objects/KernelParameters.py
+++ Processing/objects/KernelParameters.py
@@ -27,7 +27,7 @@ import typing as t
 # import os
 
 
-class KernelParameters():
+class KernelParametersGeneral():
 
     # Initialization of N of kernels, size and parameters
     def __init__(self,
@@ -97,32 +97,6 @@ class KernelParameters():
 
     # Return kernels compiled
 
-    def compile_kernels(self, SX, SY):
-
-        midX = SX >> 1
-        midY = SY >> 1
-
-        r = self.kernels['r'] * (self.kernels['R'])
-        D = np.linalg.norm(np.mgrid[-midX: midX, -midY: midY], axis=0)
-        # Ds = [D / r[k] for k in range(self.n_kernels)]
-
-        Ds = [ np.linalg.norm(np.mgrid[-midX:midX, -midY:midY], axis=0) /
-        ((self.kernels['R']+15) * self.kernels['r'][k]) for k in range(self.n_kernels) ]
-
-        def sigmoid(x):
-            return 0.5 * (np.tanh(x / 2) + 1)
-
-        def ker_f(x, a, w, b): return (
-            b * np.exp(- (x[..., None] - a)**2 / w)).sum(-1)
-
-        K = np.dstack([sigmoid(-(D-1)*10) * ker_f(D, self.kernels["a"][k], self.kernels["w"][k], self.kernels["B"][k])
-                       for k, D in zip(range(self.n_kernels), Ds)])
-
-        nK = K / np.sum(K, axis=(0, 1), keepdims=True)
-        fK = np.fft.fft2(np.fft.fftshift(nK, axes=(0, 1)), axes=(0, 1))
-
-        return fK
-
     # Connection matrix where M[i, j] = number of kernels from channel i to channel j
     def conn_from_matrix(self, connection_matrix):
         C = connection_matrix.shape[0]
@@ -171,10 +145,80 @@ class KernelParameters():
 
 
 
+class KernelParameters(KernelParametersGeneral):
 
-class LeniaKernelParameters():
+    # Initialization of N of kernels, size and parameters
+    def __init__(self,
+                 connection_matrix=None,
+                 ker_params=None
+                 ) -> None:
 
-    # INITIALIZE CALLING ANOTHER SETTER, LIKE THE OTHERS AND CHOOSE PARAMETER VALUES
+        super().__init__(connection_matrix, ker_params)
+        self.ker_f = lambda x, a, w, b: (
+                b * np.exp(- (x[..., None] - a)**2 / w)
+            ).sum(-1)
+
+    def compile_kernels(self, SX, SY):
+
+        midX = SX >> 1
+        midY = SY >> 1
+
+        r = self.kernels['r'] * (self.kernels['R'])
+        D = np.linalg.norm(np.mgrid[-midX: midX, -midY: midY], axis=0)
+        # Ds = [D / r[k] for k in range(self.n_kernels)]
+
+        Ds = [ np.linalg.norm(np.mgrid[-midX:midX, -midY:midY], axis=0) /
+        ((self.kernels['R']+15) * self.kernels['r'][k]) for k in range(self.n_kernels) ]
+
+        def sigmoid(x):
+            return 0.5 * (np.tanh(x / 2) + 1)
+
+        def ker_f(x, a, w, b): return (
+            b * np.exp(- (x[..., None] - a)**2 / w)).sum(-1)
+
+        K = np.dstack([sigmoid(-(D-1)*10) * ker_f(D, self.kernels["a"][k], self.kernels["w"][k], self.kernels["B"][k])
+                       for k, D in zip(range(self.n_kernels), Ds)])
+
+        nK = K / np.sum(K, axis=(0, 1), keepdims=True)
+        fK = np.fft.fft2(np.fft.fftshift(nK, axes=(0, 1)), axes=(0, 1))
+
+        return fK
+
+
+    def new_params(self, data):
+
+        max_rings = 0
+        for k in 'Baw':
+            temp = max(len(sublist) for sublist in data[k])
+            if temp > max_rings:
+                max_rings = temp
+
+        for k in 'Baw':
+            for B in data[k]:
+                temp = [0] * (max_rings - len(B))
+                B.extend(temp)
+
+        for k in 'rmshBaw':
+            self.kernels[k] = np.array(data[k], dtype=np.float64)
+
+        for k in 'C':
+            self.kernels[k] = np.array(data[k], dtype=np.int64)
+
+        self.kernels['T'] = data['T']
+        self.kernels['R'] = 13
+
+        self.n_kernels = len(self.kernels['r'])
+
+
+
+
+
+
+
+
+
+class LeniaKernelParameters(KernelParametersGeneral):
+
     # Initialization of N of kernels, size and parameters
 
     def __init__(self,
@@ -182,66 +226,9 @@ class LeniaKernelParameters():
                  ker_params=None
                  ) -> None:
 
-        self.generateRandomParameters(connection_matrix, ker_params)
-
-    def generateRandomParameters(self,
-                                 connection_matrix=None,
-                                 ker_params=None,
-                                 seed=101
-                                 ) -> None:
-
-        rand_gen = np.random.RandomState(seed)
+        super().__init__(connection_matrix, ker_params)
 
         self.ker_f = lambda x, m, s: np.exp(-((x-m)/s)**2 / 2)
-
-        if connection_matrix is None:
-
-            connection_matrix = np.array([
-                [1, 1, 1],
-                [1, 1, 1],
-                [1, 1, 1]
-            ])
-
-        self.n_kernels = int(connection_matrix.sum())
-
-        if ker_params is None:
-
-            self.spaces = {
-                "r": {'low': .2, 'high': 1., 'mut_std': .2, 'shape': None},
-                "B": {'low': .001, 'high': 1., 'mut_std': .2, 'shape': (3,)},
-                "w": {'low': .01, 'high': .5, 'mut_std': .2, 'shape': (3,)},
-                "a": {'low': .0, 'high': 1., 'mut_std': .2, 'shape': (3,)},
-                "m": {'low': .05, 'high': .5, 'mut_std': .2, 'shape': None},
-                "s": {'low': .001, 'high': .18, 'mut_std': .01, 'shape': None},
-                "h": {'low': .01, 'high': 1., 'mut_std': .2, 'shape': None},
-                'R': {'low': 2., 'high': 25., 'mut_std': .2, 'shape': None},
-            }
-
-            ker_params = {}
-            for k in 'rmsh':
-                ker_params[k] = np.round(rand_gen.uniform(
-                    self.spaces[k]['low'], self.spaces[k]['high'], self.n_kernels
-                ), 4)
-            for k in 'Baw':
-                ker_params[k] = np.round(rand_gen.uniform(
-                    self.spaces[k]['low'], self.spaces[k]['high'], (
-                        self.n_kernels, 3)
-                ), 4)
-
-        self.kernels = {}
-
-        for k in 'rmshBaw':
-            self.kernels[k] = ker_params[k]
-
-        self.kernels.update({
-            'R': rand_gen.uniform(self.spaces['R']['low'], self.spaces['R']['high'])
-        })
-        self.kernels['R'] = 13
-
-        # CHANGE WAY TO INPUT CONNCTIONS MATRIX
-        self.conn_from_matrix(connection_matrix)
-
-    # Return kernels compiled
 
     def compile_kernels(self, SX, SY):
 
@@ -284,24 +271,6 @@ class LeniaKernelParameters():
         # fKs = np.asarray(fKs).transpose((1, 2, 0))
 
         return fKs
-
-    # Connection matrix where M[i, j] = number of kernels from channel i to channel j
-
-    def conn_from_matrix(self, connection_matrix):
-        C = connection_matrix.shape[0]
-        c0 = []
-        c1 = [[] for _ in range(C)]
-        i = 0
-        for s in range(C):
-            for t in range(C):
-                n = connection_matrix[s, t]
-                if n:
-                    c0 = c0 + [s]*n
-                    c1[t] = c1[t] + list(range(i, i + n))
-                i += n
-
-        self.kernels['C'] = c0
-        self.kernels['T'] = c1
 
 
     def new_params(self, data):
